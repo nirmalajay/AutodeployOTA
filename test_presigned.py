2255c08989edb97d7f03c805aedb51a9d692105f
Nirmal N <nirmal@Nirmals-MacBook-Air.local>
2026-05-13 12:26:12 +0200

    update in backend

--- test_presigned.py
+++
@@ -1,7 +0,0 @@
-from minio import Minio
-client = Minio("fake-host:9000", access_key="minioadmin", secret_key="udumalpet", secure=False)
-try:
-    url = client.presigned_get_object("firmware", "test.bin")
-    print(url)
-except Exception as e:
-    print(e)
--- test_url.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = "http://localhost:9000/firmware/firmware_1.1.0.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=minioadmin%2F20260513%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260513T100003Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=c6130262006e8b2f785a1ebacd03dfdf917a1c4e69867aa2a2825128c4d53ed8"
-try:
-    r = requests.get(url)
-    print(r.status_code)
-except Exception as e:
-    print(e)
